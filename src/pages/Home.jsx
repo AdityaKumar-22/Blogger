bc10fa05f7048bf3713230c740fedb685e474867
Aditya Kumar <aditya.khiladi.22@gmail.com>
2025-05-28 11:19:39 +0530

    Updated Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import appwriteService from "../appwrite/config";
 import {Container, PostCard} from '../components'
 import { useSelector } from "react-redux";
 import login from '../assests/Login-amico.svg';
+import createPost from '../assests/createPost.svg';
 
 function Home() {
     const [posts, setPosts] = useState([])
@@ -24,8 +25,24 @@ function Home() {
             setPosts([])    
         }
     }, [Authstatus,userData])
-  
-    if (posts.length === 0) {
+
+    if(Authstatus && posts.length === 0){ //if user is logged in but has no posts
+        return (
+            <div className="w-full py-8 mt-4 text-center">
+                <Container>
+                    <div className="flex flex-wrap">
+                        <div className="p-2 w-full">
+                            <h1 className="text-3xl font-bold">
+                                <img src={createPost} alt="CreatePost illustration" className="w-1/3 mx-auto mb-4" />
+                                Add your blog !!!
+                            </h1>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        )
+    }
+    else if (posts.length === 0) { //if user is not logged in
         return (
             <div className="w-full py-8 mt-4 text-center">
                 <Container>
@@ -41,7 +58,7 @@ function Home() {
             </div>
         )
     }
-    return (
+    return ( 
         <div className='w-full py-8'>
             <Container>
                 <div className='flex flex-wrap'>
